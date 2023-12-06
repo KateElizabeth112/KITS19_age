efb3a63bd1c388620163c862e95f6c53f4b7bd67
Kate Cevora <kate.wilkinson@illumr.com>
2023-12-06 11:08:04 +0000

    updated to  910

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=10:00:00
 #PBS -l select=1:ncpus=15:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_KITS_predict_810
+#PBS -N nnUNet_KITS_predict_910
 
 cd ${PBS_O_WORKDIR}
 
@@ -15,7 +15,7 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/projects/cevora_phd/live/kits19/'
 
-datasets=("Dataset810_Age3" "Dataset811_Age3" "Dataset812_Age3")
+datasets=("Dataset910_Age4" "Dataset911_Age4" "Dataset912_Age4")
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
